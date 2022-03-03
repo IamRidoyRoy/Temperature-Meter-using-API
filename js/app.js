fc4ccb70b53f1a4e914fda3452e3465ef53d6e5c
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-03-03 14:47:08 +0600

    Show city and temperature dynamically

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 API_KEY = `49e39afe749d1ac4142aa2858e3f7ecf`;
 
 const searchTemperature = () => {
-    const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-    console.log(url);
+    const cityName = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric
+    `;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data))
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temp', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    console.log(temperature);
 }
